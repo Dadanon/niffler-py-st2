016116106c1b2a3f28584c6f4fd9a565319b82ed
Danko Bananko <gubenko@ia-group.ru>
2025-02-28 18:18:59 +0300

    Проходит 7 тестов, написать еще 3

--- niffler-e2e-tests-python/tests/conftest.py
+++ niffler-e2e-tests-python/tests/conftest.py
@@ -1,25 +1,86 @@
 import random
+import re
 
 import pytest
 from string import ascii_lowercase
 
+from playwright.sync_api import Playwright
 
-class NifflerUser:
-    username: str
-    password: str
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+from .functions import *
 
 
 @pytest.fixture
 def niffler_user():
     def _niffler_user(username_length: int = 10, password_length: int = 10):
         niffler_user: NifflerUser = NifflerUser(
-            username=''.join(random.choice(ascii_lowercase) for i in range(username_length)),
-            password=''.join(random.choice(ascii_lowercase) for i in range(password_length))
+            username=''.join(random.choice(ascii_lowercase) for _ in range(username_length)),
+            password=''.join(random.choice(ascii_lowercase) for _ in range(password_length))
         )
         return niffler_user
     yield _niffler_user
 
+
+@pytest.fixture
+def niffler_registered_user(niffler_user):
+    """Get registered Niffler user and page to do not recreate it later"""
+    def _niffler_registered_user(playwright: Playwright):
+        browser = playwright.chromium.launch()
+        page = browser.new_page()
+        page.goto(NIFFLER_FRONTEND_URL)
+        page.get_by_role('link').get_by_text('Create new account').click()
+
+        new_user = niffler_user()
+
+        username_field, password_field, password_confirm_field = page.get_by_label('Username'), page.get_by_label(
+            'Password').first, page.get_by_label('Submit password')
+        username_field.fill(new_user.username)
+        password_field.fill(new_user.password)
+        password_confirm_field.fill(new_user.password)
+        page.get_by_role('button').get_by_text('Sign up').click()
+        return new_user, page
+
+    yield _niffler_registered_user
+
+
+@pytest.fixture
+def niffler_spend():
+    """Get mock spend data"""
+    def _niffler_spend():
+        spend: NifflerSpend = NifflerSpend(
+            amount=round(random.uniform(MIN_AMOUNT, MAX_AMOUNT), 2),
+            currency=random.choice(list(Currency)),
+            category=''.join([random.choice(ascii_lowercase) for _ in range(CATEGORY_NAME_LENGTH)]),
+            date=get_random_date().strftime(SPEND_CREATE_DATE_FORMAT),
+            description=''.join([random.choice(ascii_lowercase) for _ in range(CATEGORY_NAME_LENGTH)]),
+        )
+        return spend
+
+    yield _niffler_spend
+
+
+@pytest.fixture
+def niffler_add_spend(niffler_registered_user, niffler_spend):
+    """Create registered user, login and create spend
+    :return: current page and NifflerSpend object
+    """
+    def _niffler_add_spend(playwright: Playwright):
+        registered_user, page = niffler_registered_user(playwright)
+        page.goto(NIFFLER_FRONTEND_URL)
+        login_with_user(page, registered_user)
+        page.get_by_role('link').get_by_text('New spending').click()
+
+        new_spend: NifflerSpend = niffler_spend()
+        page.get_by_label('Amount').fill(str(new_spend.amount))
+
+        page.locator('#currency').click()
+        page.locator(f'ul[role="listbox"] >> li:has-text("{new_spend.currency.value["value"]}")').click()
+
+        page.locator('#category').fill(new_spend.category)
+        page.locator('input[name="date"]').fill(new_spend.date)
+        page.get_by_label('Description').fill(new_spend.description)
+        page.get_by_role('button').get_by_text('Add').click()
+        page.wait_for_url(re.compile('main'), wait_until='load')
+        page.wait_for_selector('table')
+        return page, new_spend
+
+    yield _niffler_add_spend
--- niffler-e2e-tests-python/tests/test_auth.py
+++ niffler-e2e-tests-python/tests/test_auth.py
@@ -1,15 +1,7 @@
-import os
 import re
 
-import dotenv
-from playwright.sync_api import sync_playwright, expect
-
-
-dotenv.load_dotenv()
-
-
-NIFFLER_FRONTEND_URL = os.getenv("NIFFLER_FRONTEND_URL")
-NIFFLER_AUTH_URL = os.getenv("NIFFLER_AUTH_URL")
+from playwright.sync_api import sync_playwright, expect, Page
+from .functions import *
 
 
 def test_login_error(niffler_user):
@@ -19,7 +11,6 @@ def test_login_error(niffler_user):
     2. Enter invalid credentials
     3. Get login error
     """
-
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
@@ -28,21 +19,75 @@ def test_login_error(niffler_user):
         expect(page).to_have_url(re.compile(NIFFLER_AUTH_URL))  # Check redirection
         new_user = niffler_user()
 
-        login_field, password_field = page.get_by_label('Username'), page.get_by_label('Password')
-        login_field.fill(new_user.username)
-        password_field.fill(new_user.password)
-        page.get_by_role('button').get_by_text('Log in').click()
+        login_with_user(page, new_user)
 
         expect(page).to_have_url(re.compile('error'))
 
 
-def test_login_success(niffler_user):
+def test_register_success(niffler_user):
     """
     1. Go to main page
     2. Click register button
-    3. Register user and save
-    4. Go to main page (it will be redirected, already checked id test_login_error)
-    5. Fill fields with valid credentials and enter
-    6. Check if page is main now
+    3. Register with valid credentials (username field length 10, password field length 10)
+    4. Click submit button
+    5. Check if sign in button is visible - it means successful registration
     """
-    ...
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(NIFFLER_FRONTEND_URL)
+        page.get_by_role('link').get_by_text('Create new account').click()
+
+        new_user = niffler_user()
+
+        username_field, password_field, password_confirm_field = page.get_by_label('Username'), page.get_by_label('Password').first, page.get_by_label('Submit password')
+        username_field.fill(new_user.username)
+        password_field.fill(new_user.password)
+        password_confirm_field.fill(new_user.password)
+        page.get_by_role('button').get_by_text('Sign up').click()
+
+        expect(page.get_by_role('link').get_by_text('Sign in')).to_be_visible()
+
+
+def test_register_error(niffler_user):
+    """
+    1. Go to main page
+    2. Click register button
+    3. Register with invalid credentials (username field length 2, password field length 2)
+    4. Click submit button
+    5. Check if sign in button is visible - it means successful registration
+    """
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(NIFFLER_FRONTEND_URL)
+        page.get_by_role('link').get_by_text('Create new account').click()
+
+        new_user = niffler_user(username_length=2, password_length=2)
+
+        username_field, password_field, password_confirm_field = page.get_by_label('Username'), page.get_by_label('Password').first, page.get_by_label('Submit password')
+        username_field.fill(new_user.username)
+        password_field.fill(new_user.password)
+        password_confirm_field.fill(new_user.password)
+        page.get_by_role('button').get_by_text('Sign up').click()
+
+        expect(page.get_by_role('link').get_by_text('Sign in')).not_to_be_visible()
+
+
+def test_login_success(niffler_registered_user):
+    """
+    1. Get registered user and page from fixture
+    2. Go to main page (it will be redirected, already checked id test_login_error)
+    3. Fill fields with valid credentials and enter
+    4. Check if page is main now
+    """
+    # INFO: we have already tested registration in other test so we only have to use already registered user from fixture
+    with sync_playwright() as p:
+        registered_user, page = niffler_registered_user(p)
+        page.goto(NIFFLER_FRONTEND_URL)
+
+        login_with_user(page, registered_user)
+
+        expect(page).to_have_url(re.compile('main'))
+
+
